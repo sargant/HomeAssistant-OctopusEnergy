a9c8d527131b289d46cdaac92a46995f22d316cc
Robert Sargant <robert@sargant.com>
2026-08-25 16:54:35 +0100

    refactor: remove double device definition

--- custom_components/octopus_energy/cost_tracker/base.py
+++ custom_components/octopus_energy/cost_tracker/base.py
@@ -1,4 +1,3 @@
-from homeassistant.helpers.device import async_entity_id_to_device
 from homeassistant.helpers.device_registry import DeviceEntryType, DeviceInfo
 
 from ..const import DOMAIN
@@ -11,11 +10,6 @@ def get_cost_tracker_unique_id(tracker_name: str, peak_type = None):
 
 class BaseCostTracker:
   def __init__(self, hass, source_entity_id: str, tracker_name: str):
-    self.device_entry = async_entity_id_to_device(
-      hass,
-      source_entity_id,
-    )
-
     self._attr_has_entity_name = True
     self._attr_device_info = DeviceInfo(
       identifiers={(DOMAIN, f"cost-tracker-{tracker_name}")},
